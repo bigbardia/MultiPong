503826bbbd2876a6cbe9ac8591e2f9998f2a717e
bigbardia <bigbardia@gmail.com>
2022-09-22 16:51:14 +0330

    fixe

--- multipong/views.py
+++ multipong/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint , render_template
-from multipong.models import PublicChat
+from multipong.models import PublicChat, User
 
 views = Blueprint("views" , __name__ , template_folder="templates" , static_folder="static")
 
@@ -7,7 +7,8 @@ views = Blueprint("views" , __name__ , template_folder="templates" , static_fold
 @views.route("/")
 def index():
     context = {
-        "chats" : PublicChat.query.all()[-10:]
+        "chats" : PublicChat.query.all()[-10:],
+        "online_users" : User.query.filter(User.online > 0).count()
     }
     return render_template("index.html" , **context)
 
